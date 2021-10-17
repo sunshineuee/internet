9f0d02f936af55d6c2a4a300f944d70257f57a08
sunshineuee <61143485+sunshineuee@users.noreply.github.com>
2021-10-17 20:34:50 +0300

    lesson04

--- lesson04/ex1.py
+++ lesson04/ex1.py
@@ -1,9 +1,9 @@
-def save_news(news):
+def save_news(news, name_source):
     from pymongo import MongoClient
     client = MongoClient("127.0.0.1", 27017)
-    db = client["news_bd"]
+    db = client[name_source]
     col = db.news
-    res = [col.insert_one(el) for el in news if col.count_documents({"href": el["href"]}) == 0]
+    res = [col.insert_one(el) for el in news if col.count_documents({"link": el["link"]}) == 0]
     print(col.count_documents({}))
     return res
 
@@ -17,11 +17,20 @@ def get_news_mail():
     header = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"}
     response = requests.get(url, headers=header)
     root = html.fromstring(response.text)
-    result = root.xpath('//div[@class="daynews__item"]/a | //li/a | //li/span/a')
-    res = [{"site": site,
-            "text": el.text_content(),
-            "href": el.attrib["href"]
-            } for el in result]
+    result = root.xpath('//td[@class="daynews__main"]/div | //div[@class="daynews__item"] | //ul[@data-module]/li')
+    res = []
+    for el in result:
+        link = el.xpath('.//a/@href')[0]
+        response_link = requests.get(link, headers=header)
+        dom_news = html.fromstring(response_link.text)
+        res.append({"site": site,
+                    "source": dom_news.xpath('//span[@class="note"]//span[@class="link__text"]/text()')[0],
+                    "name": el.xpath('.//span[@class="photo__captions"]//span[1]/text() | .//a/text()')[0].replace(
+                        '\xa0', ' '),
+                    "link": el.xpath('.//a/@href')[0],
+                    "text": el.text_content(),
+                    "time": dom_news.xpath('//span[@class="note"]/span[@datetime]/@datetime')[0]
+                    })
     return res
 
 
@@ -34,13 +43,16 @@ def get_news_yandex():
     header = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"}
     response = requests.get(url, headers=header)
     root = html.fromstring(response.text)
-    result = root.xpath('//h2[@class="mg-card__title"]')
+    result = root.xpath("//article[contains(@class, 'mg-card')]")
     res = [{"site": site,
+            "source": el.xpath(".//a[@class = 'mg-card__source-link']/text()"),
+            "name": str(el.xpath(".//h2[@class = 'mg-card__title']/text()")).replace("\\xa0", " "),
+            "link": el.xpath(".//a[@class = 'mg-card__link']/@href"),
             "text": el.text,
-            "href": el.getparent().attrib["href"]
+            "time": el.xpath(".//span[@class = 'mg-card-source__time']/text()")
             } for el in result]
     return res
 
 
-save_news(get_news_mail())
-save_news(get_news_yandex())
+save_news(get_news_mail(), 'mail')
+save_news(get_news_yandex(), 'yaru')
